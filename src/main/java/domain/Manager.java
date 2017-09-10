345e86946fc8c80f58d4ad04cc7c023b288c5b54
keisogaw <keisogaw@yahoo-corp.jp>
2017-09-10 21:54:47 +0900

    first commit

---
+++ src/main/java/domain/Manager.java
@@ -0,0 +1,7 @@
+package domain;
+
+/**
+ * Created by keisogaw on 2017/09/10.
+ */
+public class Manager {
+}
---
+++ src/main/java/service/Service.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by keisogaw on 2017/09/10.
+ */
+public class Service {
+}
